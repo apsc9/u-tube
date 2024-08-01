59dc160c9db30b1c95c943d08e6ffaac1edda044
aditya <mp7020216@gmail.com>
2024-08-01 23:53:38 +0530

    Add search functionality and n-level nested comment support to the app

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,9 +1,53 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { toggleMenu } from "../utils/appSlice"
+import { useEffect, useState } from "react";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
+import { cacheResults } from "../utils/searchSlice";
 
 const Head = () => {
 
-  const dispatch = useDispatch()
+  const [searchQuery, setSearchQuery ] = useState("");
+  const [suggestions, setSuggestions ] = useState([]);
+  const [showSuggestions, setShowSuggestions ] = useState(false);
+
+  const searchCache = useSelector((store) => store.search);
+  const dispatch = useDispatch();
+  
+
+  useEffect(() => {
+
+    // make an API call after every key press
+    // but if the difference between 2 APi calls < 200ms
+    // decline the API call
+
+    const timer = setTimeout(() => {
+        if (searchCache[searchQuery]){
+            setSuggestions(searchCache[searchQuery]);
+        } else {
+            getSearchSuggestions();
+        }
+    }, 200);
+
+    return () => {
+        clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    //console.log(json[1]);
+    setSuggestions(json[1]);
+
+    // update cache
+    dispatch(
+        cacheResults({
+            [searchQuery]: json[1],
+            // iphone: [1,2,3],
+        })
+    );
+  };
+    
 
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
@@ -25,14 +69,30 @@ const Head = () => {
                 />
             </a>
         </div>
-        <div className="py-2 px-20 col-span-10">
-            <input 
-                className="w-1/2 border border-gray-400 p-2 rounded-l-full" 
-                type="text" 
-            />
-            <button className="px-5 py-2 border border-gray-400 rounded-r-full bg-gray-100">
-                    Search
-            </button>
+        <div className="relative py-2 px-20 col-span-10">
+            <div>
+                <input 
+                    className="w-1/2 border border-gray-400 p-2 px-5 rounded-l-full" 
+                    type="text"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)} 
+                    onFocus={() => setShowSuggestions(true)}
+                    onBlur={() => setShowSuggestions(false)}
+                    
+                />
+                <button className="px-5 py-2 border border-gray-400 rounded-r-full bg-gray-100">
+                        Search
+                </button>
+            </div>
+            {showSuggestions && (
+                <div className="absolute bg-white py-2 px-2 w-[37rem] rounded-lg shadow-lg border-gray-100">
+                    <ul>
+                        {suggestions.map((s) => (
+                            <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">{s}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
         <div>
             <img className="h-12"
